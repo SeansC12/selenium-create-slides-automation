eab3dee965d92e25da39f736ec1a9873b4d0c278
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2025-02-10 15:30:47 +0800

    automation creates slides

---
+++ convert.py
@@ -0,0 +1,20 @@
+import html
+
+prompt = ""
+with open('prompt.txt', 'r') as f:
+    prompt = f.read()
+
+html_text = ""
+
+for i, line in enumerate(prompt.split('\n')):
+    html_text += html.escape(line) + "\\n"
+    # if i == 0:
+    #     html_text += f"<p class=\"block block-paragraph first-block\">{html.escape(line)}</p>"
+    # elif i == len(prompt.split('\n')) - 1:
+    #     html_text += f"<p class=\"is-focused is-focused-inside block block-paragraph last-block\">{html.escape(line)}</p>"
+    # else:
+    #     html_text += f"<p class=\"block block-paragraph\">{html.escape(line)}</p>"
+
+
+with open('html.txt', 'w') as f:
+    f.write(html_text)
